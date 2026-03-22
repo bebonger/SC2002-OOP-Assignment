46012935ded20a7fc1a37d34c37570500e10dda4
bebonger <joashfeh3252@hotmail.com>
2026-03-22 22:10:55 +0800

    remove unwanted comment

--- src/main/java/sc2002/combat/ui/BattleObserver.java
+++ src/main/java/sc2002/combat/ui/BattleObserver.java
@@ -4,7 +4,7 @@ import sc2002.combat.core.entities.Entity;
 
 public interface BattleObserver {
     void onRoundStart(int roundNumber);
-    void onTurnStart(/*Entity activeEntity*/);
+    void onTurnStart(Entity activeEntity);
 
     // other events
     void onDamageDealt(Entity target, int damage, int currentHP, boolean isEliminated);
